f120ee37c1b9d97094bb2b706cb0b930e31005fb
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-05-21 09:18:09 -0300

    refactor: adicionando código em funções separadas

--- src/main.c
+++ src/main.c
@@ -1,31 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Função para converter a porcentagem da bateria em kwh
-float conversao_para_kwh(float bateria_inicial, float capacidade_bateria) {
+void exibir_estacao_recarga();
+void menu_opcoes(int opcao);
+int isBateriaValido(float bateria_inicial);
+int isCapacidadeBateriaValido(float capacidade_bateria);
+float conversao_para_kwh(float bateria_inicial, float capacidade_bateria);
+float calcular_energia_consumida(float capacidade_bateria, float bateria_kwh);
+float calcular_tempo_recarga(float energia_necessaria);
+float calcular_pagamento(float energia_necessaria);
+float carregar_bateria(float bateria_inicial);
+void relatorio_sessao();
 
-    return (bateria_inicial / 100) * capacidade_bateria;
-
-}
+float bateria_inicial;
+float bateria_final;
+float total_pagar;
+float energia_necessaria;
+float capacidade_bateria;
+float temp_recarga;
 
 int main() {
-
-    //Variáveis
-    float bateria_inicial;
-    float bateria_final;
-    float total_pagar;
-    float energia_necessaria;
-    float capacidade_bateria;
-    float temp_recarga;
     int opcao;
 
-    //Formatação de entrada
+    // Formatação de entrada
+    exibir_estacao_recarga();
+    scanf("%d", &opcao);
+    
+    menu_opcoes(opcao);
+
+    // Verificando se o valor da portcentagem da bateria é válido
+    if (!isBateriaValido(bateria_inicial)) {
+        printf("Valor de Bateria inválido!\n");
+    }
+
+    //Verificando se o valor da capacidade da bateria é válido
+    if (!isCapacidadeBateriaValido(capacidade_bateria)) {
+        printf("Capacidade inválida!\n");
+    }
+
+    //Cálculo do total de energia consumida
+    energia_necessaria = calcular_energia_consumida(capacidade_bateria, conversao_para_kwh(bateria_inicial, capacidade_bateria));
+
+    //Tempo de recarga estimado, Potência do carregador: 75kW  
+    temp_recarga = calcular_tempo_recarga(energia_necessaria);
+
+    //Cálculo da cobrança do carregamento, preço médio do KWh em SP é R$0,80
+    total_pagar = calcular_pagamento(energia_necessaria);
+    
+    // Carregamento da bateria
+    bateria_final = carregar_bateria(bateria_inicial);
+
+    //Formatação da saída e registros da sessão de recarga
+    relatorio_sessao();
+    
+    return 0;
+}
+
+void exibir_estacao_recarga() {
     printf("=================================\n");
     printf("        Estacao De Recarga\n");
     printf("1 - Iniciar recarga - 2 - Sair: ");
-    scanf("%d", &opcao);
-    if (opcao == 1)
+}
+
+void menu_opcoes(int opcao) {
+    switch (opcao)
     {
+    case 1:
         printf("=================================\n");
         printf("         Sessao iniciada\n");
         printf("Conecte O Carregador Ao Seu Carro\n");
@@ -34,49 +74,50 @@ int main() {
         printf("Digite a porcentagem da bateria do seu carro: ");
         scanf("%f", &bateria_inicial);
         printf("=================================\n");
-
-    }else if (opcao == 2)
-    {
+        break;
+    case 2:
         exit(0);
+        break;
+    default:
+        printf("Valor inválido");
+        break;
     }
-    else
+}
+
+int isBateriaValido(float bateria_inicial) {
+    return bateria_inicial > 0 ||  bateria_inicial < 100;
+}
+
+int isCapacidadeBateriaValido(float capacidade_bateria) {
+    return capacidade_bateria >= 0;
+}
+
+//Função para converter a porcentagem da bateria em kwh
+float conversao_para_kwh(float bateria_inicial, float capacidade_bateria) {
+    return (bateria_inicial / 100) * capacidade_bateria;
+}
+
+float calcular_energia_consumida(float capacidade_bateria, float bateria_kwh) {
+    return capacidade_bateria - bateria_kwh;
+}
+
+float calcular_tempo_recarga(float energia_necessaria) {
+    return (energia_necessaria / 75) * 60;
+}
+
+float calcular_pagamento(float energia_necessaria) {
+    return energia_necessaria * 0.8;
+}
+
+float carregar_bateria(float bateria_inicial) {
+    while (bateria_inicial < 100)
     {
-        printf("Valor invalido\n");
-        return 0;
+        bateria_inicial++;
     }
+    return bateria_inicial;
+}
 
-    //Verificando se o valor da portcentagem da bateria é válido
-    if (bateria_inicial < 0 ||  bateria_inicial > 100)
-    {
-        printf("Valor Invalido!");
-        return 0;
-    }
-
-    //Verificando se o valor da capacidade da bateria é válido
-    if (capacidade_bateria <= 0)
-    {
-    printf("Capacidade invalida!\n");
-    return 0;
-    }
-
-    //Cálculo do total de energia consumida
-    energia_necessaria = capacidade_bateria - conversao_para_kwh(bateria_inicial, capacidade_bateria);
-
-    //Tempo de recarga estimado, Potência do carregador: 75kW  
-    temp_recarga = (energia_necessaria / 75) * 60;
-
-
-    //Cálculo da cobrança do carregamento, preço médio do KWh em SP é R$0,80
-    total_pagar = energia_necessaria * 0.8;
-    
-    //Carregamento da bateria
-    bateria_final = bateria_inicial;
-    while (bateria_final < 100)
-    {
-        bateria_final ++;
-    }
-
-    //Formatação da saída e registros da sessão de recarga
+void relatorio_sessao() {
     printf("=============================================\n");
     printf("             Relatorio Da Sessao\n");
     printf("Carga inicial da bateria: %.2f%%\n", bateria_inicial);
@@ -86,6 +127,4 @@ int main() {
     printf("Tempo de recarga estimado: %.0f min\n", temp_recarga);
     printf("              Sessao finalizada\n");
     printf("=============================================");
-    
-    return 0;
 }
